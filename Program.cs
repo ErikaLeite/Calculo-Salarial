ff24e5e84cf316421310d4656800859e6960c186
ERIKA LEITE PAIVA <you@example.com>
2020-11-13 11:09:23 -0300

    Programa iniciado

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculo_Salarial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
